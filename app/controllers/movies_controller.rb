eb4d4b0a14d723fee6773cdb2c237b3166d63af0
Anders <kiss7medical@gmail.com>
2019-09-22 13:45:16 -0400

    edit index view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,9 +10,16 @@ class MoviesController < ApplicationController
 
 	def create
 		@movie = Movie.new(movie_params)
+
+		if @movie.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
 	end
 
 	def show
+
 	end
 
 	private
